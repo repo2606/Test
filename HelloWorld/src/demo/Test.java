b3f50d9a6d38e9a9db2d778da20e083f30e81490
David <learn-git-fast@abc.com>
2024-12-05 17:22:53 -0500

    initial commit

---
+++ HelloWorld/src/demo/Test.java
@@ -0,0 +1,14 @@
+package demo;
+
+public class Test {
+	
+	private static String str = "welcome";
+
+	public static void main(String[] args) {
+    
+		
+		System.out.println("length of string is :"+str.length());
+
+	}
+
+}
